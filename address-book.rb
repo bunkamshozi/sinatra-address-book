10586ed9073515fd827c8a871b801f4c2b6d2316
Gift Mngqibisa <bunka.mshozi@gmail.com>
2013-09-05 15:53:40 -0400

    Generate HTML using Slim

--- address-book.rb
+++ address-book.rb
@@ -1,7 +1,9 @@
 require 'sinatra/base'
+require 'slim'
 
 class AddressBook < Sinatra::Base
   get '/' do
-    'Hello World. Are you there?'
+    #'Hello World. Are you there?'
+    slim :home
   end
 end
