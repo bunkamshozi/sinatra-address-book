a1b6cb5d63aabb1ed95fb0814d35ac6f2f098dc9
Gift Mngqibisa <bunka.mshozi@gmail.com>
2013-09-05 16:37:02 -0400

    Added Testing with RSpec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+$: << File.expand_path('../..', __FILE__)
+
+require 'address-book'
+require 'rack/test'
+
+
+def app
+  AddressBook.new
+end
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+end
