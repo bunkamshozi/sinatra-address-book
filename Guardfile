7556b3beffacc08a6ebaef34b8a96b38bb051570
Gift Mngqibisa <gift.mngqibisa@maine.edu>
2018-03-07 19:55:09 -0500

    Fixed issues with ruby format

--- Guardfile
+++ Guardfile
@@ -1,3 +1,3 @@
-guard :unicorn, :daemonize => true do
+guard :unicorn, daemonize: true do
   `git ls-files`.each_line { |s| s.chomp!; watch s }
 end
--- address-book.rb
+++ address-book.rb
@@ -1,9 +1,10 @@
 require 'sinatra/base'
 require 'slim'
 
+# Setup Address Book
 class AddressBook < Sinatra::Base
   get '/' do
-    #'Hello World. Are you there?'
+    # 'Hello World. Are you there?'
     slim :home
   end
 end
